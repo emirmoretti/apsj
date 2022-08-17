4aea82e97b9109a05ec6558068e12500886f7c2e
emirmoretti <emir.moretti@hotmail.com.ar>
2022-08-16 22:11:31 -0300

    bg

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,31 +6,33 @@ const Footer = () => {
     <footer class="bg-dark py-5">
       <div class="container">
         <div class="row align-items-top text-center text-md-left text-white">
-          <div class="col-12 col-sm-6 col-md-4">
+          <div class="col-12 col-sm-6 col-md-4 ">
             <h3>
               <strong>Menu</strong>
             </h3>
-            <Link to="/" className="nav-link">
-              Inicio
-            </Link>
-            <Link to="/nosotros" className="nav-link">
-              Nosotros
-            </Link>
-            <Link to="/devocion" className="nav-link">
-              Devoción
-            </Link>
-            <Link to="/oraciones" className="nav-link">
-              Oraciones
-            </Link>
-            <Link to="/consagracion" className="nav-link">
-              Consagración
-            </Link>
-            <Link to="/llamados" className="nav-link">
-              Llamados
-            </Link>
-            <Link to="/contacto" className="nav-link">
-              Contacto
-            </Link>
+            <nav class="nav flex-column ">
+              <Link to="/" className="nav-link">
+                Inicio
+              </Link>
+              <Link to="/nosotros" className="nav-link">
+                Nosotros
+              </Link>
+              <Link to="/devocion" className="nav-link">
+                Devoción
+              </Link>
+              <Link to="/oraciones" className="nav-link">
+                Oraciones
+              </Link>
+              <Link to="/consagracion" className="nav-link">
+                Consagración
+              </Link>
+              <Link to="/llamados" className="nav-link">
+                Llamados
+              </Link>
+              <Link to="/contacto" className="nav-link">
+                Contacto
+              </Link>
+            </nav>
           </div>
 
           <div class="col-12 col-sm-6 col-md-4 mt-4 mt-sm-0">
