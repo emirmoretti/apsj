d22c756b758753a22c2e4eba4dffa1ea7a9cc015
emirmoretti <emir.moretti@hotmail.com.ar>
2022-08-19 01:35:43 -0300

    email js

--- src/components/contact/ContactForm.jsx
+++ src/components/contact/ContactForm.jsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "./ContactForm.scss";
+import emailjs from "@emailjs/browser";
 
 const ContactForm = () => {
+  const form = useRef();
+
+  const service = "service_pi7364d";
+  const template = "template_rfeboji";
+  const key = "eY-3YmGIl4Z7sH-yN";
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(service, template, form.current, key).then(
+      (result) => {
+        alert("Mensaje enviado con exito.");
+        form.current.reset();
+      },
+      (error) => {
+        alert("Error al enviar mensaje.");
+      }
+    );
+  };
+
   return (
     <section id="contact-section">
       <div data-aos="fade-right" data-aos-duration="2000" data-aos-easing="ease-in-sine">
@@ -43,36 +64,36 @@ const ContactForm = () => {
             </Col>
             <Col md={8}>
               <h4>Contacto</h4>
-              <form>
+              <form ref={form} onSubmit={sendEmail}>
                 <Row>
                   <Col className="mb-4" md={6}>
                     <div className="form-group">
-                      <input placeholder="Nombre" className="form-control"></input>
+                      <input name="nombre" type="text" placeholder="Nombre" className="form-control" required></input>
                     </div>
                   </Col>
                   <Col className="" md={6}>
                     <div className="form-group">
-                      <input className="form-control" placeholder="Apellido"></input>
+                      <input name="apellido" type="text" className="form-control" placeholder="Apellido" required></input>
                     </div>
                   </Col>
                   <Col className="mb-3" md={6}>
                     <div className="form-group">
-                      <input placeholder="Teléfono" className="form-control"></input>
+                      <input name="telefono" type="number" inputMode="numeric" placeholder="Teléfono" className="form-control" required></input>
                     </div>
                   </Col>
                   <Col className="" md={6}>
                     <div className="form-group">
-                      <input className="form-control" placeholder="Email"></input>
+                      <input name="email" type="email" className="form-control" placeholder="Email" required></input>
                     </div>
                   </Col>
                   <Col className="" md={12}>
                     <div className="form-group">
-                      <input className="form-control" placeholder="Provincia"></input>
+                      <input name="provincia" type="text" className="form-control" placeholder="Provincia" required></input>
                     </div>
                   </Col>
                   <Col className="mt-3" md={12}>
                     <div className="form-group">
-                      <textarea className="form-control" placeholder="Mensaje aquí" rows={4}></textarea>
+                      <textarea name="mensaje" type="text" className="form-control" placeholder="Mensaje aquí" rows={4} required></textarea>
                     </div>
                   </Col>
                 </Row>
